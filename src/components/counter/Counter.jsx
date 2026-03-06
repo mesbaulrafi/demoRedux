f0cd3b7fb0e8db5e3ba3990638ec063fd04f079a
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-03-07 01:05:39 +0600

    edit

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -16,13 +16,7 @@ export function Counter() {
         >
           Increment
         </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
+       
       </div>
     </div>
   )
